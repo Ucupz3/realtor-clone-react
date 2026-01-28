c5490c8ab5eee63825697921ca9a1bd4c4bb2af3
Ucupz3 <yusufrickyhartono09@gmail.com>
2026-01-28 20:38:09 +0700

    creates page and routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Profile from './pages/profile';
+import Offers from './pages/offers';
+import ForgotPassword from './pages/forgotpassword';
+import Signin from './pages/signin';
+import Signup from './pages/signup';
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">Hello World</h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/offers" element={<Offers />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
